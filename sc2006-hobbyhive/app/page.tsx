486c7a5cef73702915552825141f129c6eeb8183
rong xuan <51030705+RongXuaan@users.noreply.github.com>
2025-09-17 13:22:56 +0800

    Created nextjs environment
    created base nextjs environment with typescript and tailwind (dummy template)

---
+++ sc2006-hobbyhive/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-align-center">
+      <div>
+        <p className="text-red-500 text-centerg text-8xl">hello</p>
+      </div>
+      </main>
+  );
+}
