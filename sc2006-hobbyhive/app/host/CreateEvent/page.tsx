002181ba7783544efc9df4895e51d2d195c41abc
Swetha-aaa <167495735+Swetha-aaa@users.noreply.github.com>
2025-10-29 17:53:10 +0800

    Update page.tsx

--- sc2006-hobbyhive/app/host/CreateEvent/page.tsx
+++ sc2006-hobbyhive/app/host/CreateEvent/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createClient } from "@/utils/supabase/client";
 import Header from "../../components/header";
 import Navbar from "../../components/Navbar";
