988afcba0173baa6ea4e5141f2b703872b1dab09
Swetha-aaa <167495735+Swetha-aaa@users.noreply.github.com>
2025-11-11 15:26:26 +0800

    Update EventModel.ts

--- sc2006-hobbyhive/app/host/models/EventModel.ts
+++ sc2006-hobbyhive/app/host/models/EventModel.ts
@@ -1,1 +1,16 @@
-
+export interface EventData {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  latitude?: number;
+  longitude?: number;
+  nearest_mrt_station?: string;
+  nearest_mrt_distance?: number;
+  description: string;
+  capacity: string;
+  category: string;
+  skillLevel: string;
+  image_url?: string;
+}
