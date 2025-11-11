7fdf7b06d835d4db06bd8a39c7bd41737cba6765
Swetha-aaa <167495735+Swetha-aaa@users.noreply.github.com>
2025-11-11 15:17:51 +0800

    Update EventForm.tsx

--- sc2006-hobbyhive/app/host/components/EventForm.tsx
+++ sc2006-hobbyhive/app/host/components/EventForm.tsx
@@ -1,3 +1,26 @@
 "use client";
 import React from "react";
 
+interface EventFormProps {
+  title: string;
+  setTitle: (title: string) => void;
+  date: string;
+  setDate: (date: string) => void;
+  time: string;
+  setTime: (time: string) => void;
+  description: string;
+  setDescription: (description: string) => void;
+  capacity: string;
+  setCapacity: (capacity: string) => void;
+  category: string;
+  setCategory: (category: string) => void;
+  skillLevel: string;
+  setSkillLevel: (skillLevel: string) => void;
+  imageFile: File | null;
+  setImageFile: (file: File | null) => void;
+  errors: { [key: string]: boolean };
+  isLoading: boolean;
+  isEditing: boolean;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  getInputClassName: (fieldName: string) => string;
+}
